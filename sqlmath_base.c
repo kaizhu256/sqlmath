5e1161ba350827670bf42b1ec0dcfa9298d314d9
Kai Zhu <kaizhu256@gmail.com>
2023-04-24 14:19:36 -0500

    move.ext.compress

--- sqlmath_base.c
+++ sqlmath_base.c
@@ -393,6 +393,12 @@ shRawLibFetch
     ],
     "replaceList": [
         {
+            "aa": "\\bunsigned long(?: int)?\\b",
+            "bb": "uint32_t",
+            "flags": "g",
+            "substr": ""
+        },
+        {
             "aa": "^(?:#include .*?|SQLITE_EXTENSION_INIT1)$",
             "bb": "//$&",
             "flags": "gm",
@@ -400,24 +406,10 @@ shRawLibFetch
         }
     ]
 }
-
--  unsigned int nIn;
+-    int nArg = 0;               /\\* Number of arguments that seriesFilter() expects *\\/
 +// hack-sqlite - fix warning
-+  int nIn;
--  unsigned int nIn;
-+// hack-sqlite - fix warning
-+  int nIn;
-
--#include <zlib.h>
-+// hack-sqlite - inline zlib.h
-+// #include <zlib.h>
-+#ifdef EMSCRIPTEN
-+#define Z_OK 0
-+int compress(unsigned char *, unsigned long *, const unsigned char *, unsigned long);
-+int uncompress(unsigned char *, unsigned long *, const unsigned char *, unsigned long);
-+#else // EMSCRIPTEN
-+#include <zlib.h>
-+#endif // EMSCRIPTEN
++    int nArg = 0;
++/\\* Number of arguments that seriesFilter() expects *\\/
 
 -  pIn = sqlite3_value_blob(argv[0]);
 -  nIn = sqlite3_value_bytes(argv[0]);
@@ -432,6 +424,24 @@ shRawLibFetch
 +// hack-sqlite - handle null-case uncompress
 +  if (pIn == NULL) { sqlite3_result_error(context, "Cannot uncompress() NULL blob", -1); return; }
 +  nIn = sqlite3_value_bytes(argv[0]);
+
+-  unsigned int nIn;
++// hack-sqlite - fix warning
++  int nIn;
+-  unsigned int nIn;
++// hack-sqlite - fix warning
++  int nIn;
+
+-//#include <zlib.h>
++// hack-sqlite - inline zlib.h
++// #include <zlib.h>
++#ifdef EMSCRIPTEN
++#define Z_OK 0
++int compress(unsigned char *, uint32_t *, const unsigned char *, uint32_t);
++int uncompress(unsigned char *, uint32_t *, const unsigned char *, uint32_t);
++#else // EMSCRIPTEN
++#include <zlib.h>
++#endif // EMSCRIPTEN
 */
 
 
@@ -452,6 +462,177 @@ a legal notice, here is a blessing:
   *   May you find forgiveness for yourself and forgive others.
   *   May you share freely, never taking more than you give.
 */
+
+
+/*
+file https://github.com/sqlite/sqlite/blob/version-3.39.4/ext/misc/compress.c
+*/
+/*
+** 2014-06-13
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+******************************************************************************
+**
+** This SQLite extension implements SQL compression functions
+** compress() and uncompress() using ZLIB.
+*/
+//#include "sqlite3ext.h"
+//SQLITE_EXTENSION_INIT1
+// hack-sqlite - inline zlib.h
+// #include <zlib.h>
+#ifdef EMSCRIPTEN
+#define Z_OK 0
+int compress(
+    unsigned char *,
+    uint32_t *,
+    const unsigned char *,
+    uint32_t
+);
+int uncompress(
+    unsigned char *,
+    uint32_t *,
+    const unsigned char *,
+    uint32_t
+);
+#else                           // EMSCRIPTEN
+#include <zlib.h>
+#endif                          // EMSCRIPTEN
+
+/*
+** Implementation of the "compress(X)" SQL function.  The input X is
+** compressed using zLib and the output is returned.
+**
+** The output is a BLOB that begins with a variable-length integer that
+** is the input size in bytes (the size of X before compression).  The
+** variable-length integer is implemented as 1 to 5 bytes.  There are
+** seven bits per integer stored in the lower seven bits of each byte.
+** More significant bits occur first.  The most significant bit (0x80)
+** is a flag to indicate the end of the integer.
+**
+** This function, SQLAR, and ZIP all use the same "deflate" compression
+** algorithm, but each is subtly different:
+**
+**   *  ZIP uses raw deflate.
+**
+**   *  SQLAR uses the "zlib format" which is raw deflate with a two-byte
+**      algorithm-identification header and a four-byte checksum at the end.
+**
+**   *  This utility uses the "zlib format" like SQLAR, but adds the variable-
+**      length integer uncompressed size value at the beginning.
+**
+** This function might be extended in the future to support compression
+** formats other than deflate, by providing a different algorithm-id
+** mark following the variable-length integer size parameter.
+*/
+static void compressFunc(
+    sqlite3_context * context,
+    int argc,
+    sqlite3_value ** argv
+) {
+    const unsigned char *pIn;
+    unsigned char *pOut;
+// hack-sqlite - fix warning
+    int nIn;
+    unsigned long int nOut;
+    unsigned char x[8];
+    int rc;
+    int i,
+     j;
+
+    pIn = sqlite3_value_blob(argv[0]);
+// hack-sqlite - handle null-case compress
+    if (pIn == NULL) {
+        sqlite3_result_error(context, "Cannot compress() NULL blob", -1);
+        return;
+    }
+    nIn = sqlite3_value_bytes(argv[0]);
+    nOut = 13 + nIn + (nIn + 999) / 1000;
+    pOut = sqlite3_malloc(nOut + 5);
+    for (i = 4; i >= 0; i--) {
+        x[i] = (nIn >> (7 * (4 - i))) & 0x7f;
+    }
+    for (i = 0; i < 4 && x[i] == 0; i++) {
+    }
+    for (j = 0; i <= 4; i++, j++)
+        pOut[j] = x[i];
+    pOut[j - 1] |= 0x80;
+    rc = compress(&pOut[j], &nOut, pIn, nIn);
+    if (rc == Z_OK) {
+        sqlite3_result_blob(context, pOut, nOut + j, sqlite3_free);
+    } else {
+        sqlite3_free(pOut);
+    }
+}
+
+/*
+** Implementation of the "uncompress(X)" SQL function.  The argument X
+** is a blob which was obtained from compress(Y).  The output will be
+** the value Y.
+*/
+static void uncompressFunc(
+    sqlite3_context * context,
+    int argc,
+    sqlite3_value ** argv
+) {
+    const unsigned char *pIn;
+    unsigned char *pOut;
+// hack-sqlite - fix warning
+    int nIn;
+    unsigned long int nOut;
+    int rc;
+    int i;
+
+    pIn = sqlite3_value_blob(argv[0]);
+// hack-sqlite - handle null-case uncompress
+    if (pIn == NULL) {
+        sqlite3_result_error(context, "Cannot uncompress() NULL blob", -1);
+        return;
+    }
+    nIn = sqlite3_value_bytes(argv[0]);
+    nOut = 0;
+    for (i = 0; i < nIn && i < 5; i++) {
+        nOut = (nOut << 7) | (pIn[i] & 0x7f);
+        if ((pIn[i] & 0x80) != 0) {
+            i++;
+            break;
+        }
+    }
+    pOut = sqlite3_malloc(nOut + 1);
+    rc = uncompress(pOut, &nOut, &pIn[i], nIn - i);
+    if (rc == Z_OK) {
+        sqlite3_result_blob(context, pOut, nOut, sqlite3_free);
+    } else {
+        sqlite3_free(pOut);
+    }
+}
+
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+int sqlite3_compress_init(
+    sqlite3 * db,
+    char **pzErrMsg,
+    const sqlite3_api_routines * pApi
+) {
+    int rc = SQLITE_OK;
+    SQLITE_EXTENSION_INIT2(pApi);
+    (void) pzErrMsg;            /* Unused parameter */
+    rc = sqlite3_create_function(db, "compress", 1,
+        SQLITE_UTF8 | SQLITE_INNOCUOUS, 0, compressFunc, 0, 0);
+    if (rc == SQLITE_OK) {
+        rc = sqlite3_create_function(db, "uncompress", 1,
+            SQLITE_UTF8 | SQLITE_INNOCUOUS | SQLITE_DETERMINISTIC,
+            0, uncompressFunc, 0, 0);
+    }
+    return rc;
+}
 
 
 /*
@@ -892,8 +1073,7 @@ static int seriesBestIndex(
     int idxNum = 0;             /* The query plan bitmask */
     int bStartSeen = 0;         /* EQ constraint seen on the START column */
     int unusableMask = 0;       /* Mask of unusable constraints */
-    int nArg = 0;
-    /* Number of arguments that seriesFilter() expects */
+    int nArg = 0;               /* Number of arguments that seriesFilter() expects */
     int aIdx[3];                /* Constraints on start, stop, and step */
     const struct sqlite3_index_constraint *pConstraint;
 
