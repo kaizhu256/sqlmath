19e9e78b6f510ae74cf95f413d8dc4d2d147e412
kai zhu <kaizhu256@gmail.com>
2023-10-15 21:43:20 -0500

    slushy

--- sqlmath_wrapper_lightgbm.c
+++ sqlmath_wrapper_lightgbm.c
@@ -36,12 +36,10 @@ gcc -Isqlite-autoconf-3420000/ -shared sqlmath_wrapper_lightgbm.c -o build/_lgb.
 //!! #include "./lgb_c_api.h"
 SQLITE_EXTENSION_INIT1;
 
-#ifdef _WIN32
-__declspec(dllexport)
-#endif
-//!! __declspec(dllimport) int LGBM_DatasetFree(
+//!! int LGBM_DatasetFree(
     //!! DatasetHandle handle
 //!! );
+
 #define SQLMATH_FUNC
 #define SQLITE3_CREATE_FUNCTION1(func, argc) \
     errcode = sqlite3_create_function(db, #func, argc, \
