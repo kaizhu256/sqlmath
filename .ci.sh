06d88f4d88c398f2afd68eebd503eb23e69c0a1a
kai zhu <kaizhu256@gmail.com>
2024-06-03 00:44:03 -0500

    - sqlmath - Remove little-used sql-function fill_forward().

--- .ci.sh
+++ .ci.sh
@@ -179,7 +179,7 @@ shCiBaseCustomArtifactUpload() {(set -e
         rm -f "branch-$GITHUB_BRANCH0/"*.tar.gz
         cp ../../dist/sqlmath-*.tar.gz "branch-$GITHUB_BRANCH0"
         ;;
-    MINGW64_NT*)
+    MINGW*)
         rm -f "branch-$GITHUB_BRANCH0/"*-win*
         rm -f "branch-$GITHUB_BRANCH0/"*_win*
         ;;
@@ -498,11 +498,20 @@ shSqlmathUpdate() {(set -e
     . "$HOME/myci2.sh" : && shMyciUpdate
     if [ "$PWD/" = "$HOME/Documents/sqlmath/" ]
     then
+        # shRollupFetch
         shRollupFetch asset_sqlmath_external_rollup.js
         shRollupFetch index.html
         shRollupFetch sqlmath_base.h
         shRollupFetch sqlmath_external_sqlite.c
         shRollupFetch sqlmath_external_zlib.c
+        # shIndentC
+        if (uname | grep -q "MING\|MSYS")
+        then
+            shIndentC sqlmath_base.c
+            shIndentC sqlmath_custom.c
+            shIndentC sqlmath_external_sqlite.c
+            shIndentC sqlmath_external_zlib.c
+        fi
         return
     fi
     if [ -d "$HOME/Documents/sqlmath/" ]
