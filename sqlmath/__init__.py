b22d7c9ef69f8f7684846a10e0122de895187c6e
kai zhu <kaizhu256@gmail.com>
2025-07-28 22:38:50 -0500

    - python - Add support for PEP 703 – Making the Global Interpreter Lock Optional in CPython.

--- sqlmath/__init__.py
+++ sqlmath/__init__.py
@@ -21,8 +21,8 @@
 
 """sqlmath.py."""
 
-__version__ = "2025.6.28"
-__version_info__ = ("2025", "6", "28")
+__version__ = "2025.7.1"
+__version_info__ = ("2025", "7", "1")
 
 import json
 import math
